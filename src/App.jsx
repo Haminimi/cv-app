263c023fbee8c3898527950499ad2d7d7e44f3ce
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-16 19:41:33 +0100

    Declare the form state variable

--- src/App.jsx
+++ src/App.jsx
@@ -3,3 +3,17 @@ import Information from './components/Information';
 import Education from './components/Education';
 import Experience from './components/Experience';
 import './styles/App.css';
+
+export default function App() {
+	const [form, setForm] = useState({
+		information: {
+			name: '',
+			email: '',
+			number: '',
+		},
+		education: [{ school: '', title: '', finished: '' }],
+		experience: [
+			{ company: '', position: '', from: '', until: '', resp: '' },
+		],
+	});
+}
