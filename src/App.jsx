bf06bcd636d9800b8b97175969c349c272597a66
Hamza Mujinovic <mujinovichamza@gmail.com>
2024-01-16 19:40:42 +0100

    Import the components and styles

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import { useState } from 'react';
+import Information from './components/Information';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import './styles/App.css';
